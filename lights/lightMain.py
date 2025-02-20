5ae1ee8bc0cc08c3dbd2ba4e539f1b9db2ddeb91
RulleRullstol <75090562+RulleRullstol@users.noreply.github.com>
2025-02-20 20:26:59 +0100

    felsök

--- lights/lightMain.py
+++ lights/lightMain.py
@@ -10,11 +10,11 @@ if parent_dir not in sys.path:
 
 from configHelper import getLights, getDictValues, mergeDicts, getValue, getHAheaders
 
-def main(crude_list: list):
+def main(input: list):
 
     #[id, state ('on'/'off), Tuple (R, G, B), brighness (0-100)]
     #[alias, true/false, [0,0,0], int]
-
+    crude_list = input['actions']
     lights = getLights() # Iterera över konfigurerade lampor för att hitta korrekt lampa
     lconf= {}  # Lampans inställningar
     for light in lights: #
