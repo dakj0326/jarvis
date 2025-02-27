3f0c563f67b5e8ce5cdc3b73911df8f88cea6c79
RulleRullstol <75090562+RulleRullstol@users.noreply.github.com>
2025-02-27 03:13:53 +0100

    fix import

--- loop/main.py
+++ loop/main.py
@@ -7,7 +7,7 @@ parent_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
 if parent_dir not in sys.path:
     sys.path.insert(0, parent_dir)
 
-from ollama import jarvis as j
+from llm import jarvis as j
 from lights import lightMain
 
 jarvis = j.Jarvis()
